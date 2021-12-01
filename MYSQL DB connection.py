a457d2fe9d4390c1a07454bec840860b7542a342
dkansagra <89031049+dkansagra@users.noreply.github.com>
2021-11-30 22:48:08 -0500

    Corrected code for second query

--- MYSQL DB connection.py
+++ MYSQL DB connection.py
@@ -8,7 +8,7 @@ dbconn = my.connect (host='127.0.0.1',port=3306,user='root', password='',  db='c
  
 #print(dbconn) #This is a testing stub to make sure the connection worked. Uncomment it to use it.
 cursor = dbconn.cursor()
-query = "SELECT FirstName, LastName, DOB, Vac_status from covid_info join patient_demo on patient_demo.MRN = covid_info.patient_demo_MRN"
+query = "SELECT FirstName, LastName, DOB, MRN, Vac_status from covid_info join patient_demo on patient_demo.MRN = covid_info.patient_demo_MRN"
 cursor.execute(query)
 #the following line shows the schema descriptions of the headers of the data retrieved in the cursor object from the database using the query
 print(cursor.description)
@@ -16,9 +16,9 @@ print(cursor.description)
 
 users = []
 
-for (FirstName, LastName, DOB, Vac_status) in cursor:
-    print("{} {} ({}) found. Vaccination Status {}.".format(FirstName, LastName, DOB, Vac_status))
-    thisuser = [FirstName, LastName, DOB, Vac_status]
+for (FirstName, LastName, DOB, MRN, Vac_status) in cursor:
+    print("{} {} ({}) ({}) found. Vaccination Status {}.".format(FirstName, LastName, DOB, MRN, Vac_status))
+    thisuser = [FirstName, LastName, DOB, MRN, Vac_status]
     users.append(thisuser)
 cursor.close()
 cursor = dbconn.cursor()
@@ -27,7 +27,7 @@ cursor.execute(query)
 vaccine = []
 for (Vac_status, first_dose, second_dose, third_dose,patient_demo_MRN) in cursor:
     vac_dose = [Vac_status, first_dose, second_dose, third_dose,patient_demo_MRN]
-    vac_dose.append(vac_dose)
+    vaccine.append(vac_dose)
 dbconn.close()
 
 print(users)
